0f995bd2711c9eba4f9b1dd8bb08634c8e1d9824
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-02-02 16:16:18 -0700

    replaced img with next/image

--- components/NavProfile/NavProfile.tsx
+++ components/NavProfile/NavProfile.tsx
@@ -5,8 +5,10 @@ const NavProfile = () => {
     const user = useAuthContext();
     return(
         <div style={{display:"flex", flexDirection:"column", alignItems:"center"}}> 
-            <div style={{height:100, width:100, borderRadius:"100%", border:"3px solid white"}}>
-                {user?.photoURL ? <Image src={user.photoURL} alt="User Profile"></Image>
+            <div style={{height:100, width:100, borderRadius:"100%", border:"3px solid white", overflow:"hidden"}}>
+                {user?.photoURL ? <Image src={user.photoURL} alt="User Profile" height={100}
+                width={100}
+                objectFit="contain"></Image>
                 :
                 
                 <Image src="/defaultuser.svg" alt="Default User"
