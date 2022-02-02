d388af8b4acaff555f8ff8b727ac5727580d91ef
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-02-02 10:50:02 -0700

    added logic to not display current date items

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,24 +1,25 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { auth } from "../db/firebase-config";
 
-import { ITodoItem } from "../common/types";
+import { isTimestamp, ITodoItem } from "../common/types";
 import { useState, useEffect } from "react";
 import {
   getOrderedTodosByDeadline,
   getOrderedTodosByPrio,
+  getPastDeadLine,
 } from "../common/calls";
 import ToDoItem from "../components/ToDoItem/ToDoItem";
-import Skeleton from '@mui/material/Skeleton'
 import Container from "@mui/material/Container";
 import ToDoSkeleton from "../components/Skeleton/ToDoSkeleton";
 import { useAuthContext } from "../common/context";
+import { Timestamp } from "firebase/firestore";
 
 const Home: NextPage = () => {
-
   const [nearDeadline, setNearDeadline] = useState<ITodoItem[]>();
   const [topPrio, setTopPrio] = useState<ITodoItem[]>();
+  const [pastDeadline, setPastDeadline] = useState<ITodoItem[]>();
   const user = useAuthContext();
+  const currTime = Timestamp.now();
   const getNearDeadLine = async () => {
     const data = await getOrderedTodosByDeadline();
     setNearDeadline(data);
@@ -27,51 +28,77 @@ const Home: NextPage = () => {
     const data = await getOrderedTodosByPrio();
     setTopPrio(data);
   };
+  const getPastDue = async () => {
+    const data = await getPastDeadLine();
+    setPastDeadline(data);
+  };
   const getData = () => {
     getNearDeadLine();
     getTopPrio();
+    getPastDue();
   };
   useEffect(() => {
-    if(user)
-    {
-      setTimeout(getData, 1000);
+    if (user) {
+      setTimeout(getData, 1500);
     }
-    
   }, []);
 
   return (
     <Container>
-      {user?
-            <>
-            <Head>
-              <title>Home</title>
-            </Head>
-            <div>
-              <h1>Past Deadline</h1>
-            </div>
-            <div>
-              <h1>High Priority Actions</h1>
-              {topPrio
-                ? topPrio.map((item) => {
-                    return <ToDoItem key={item.id} todoItem={item}></ToDoItem>;
-                  })
-                : <ToDoSkeleton height={55} num={3}/>}
-            </div>
-            <div>
-              <h1>Upcoming Deadlines</h1>
-              {nearDeadline?
+      {user ? (
+        <>
+          <Head>
+            <title>Home</title>
+          </Head>
+          <div>
+            {pastDeadline ? (
+              <>
+                {pastDeadline.length > 0 ? <h1>Past Deadline</h1> : null}
+
+                {pastDeadline.map((item) => {
+                  return <ToDoItem key={item.id} todoItem={item}></ToDoItem>;
+                })}
+              </>
+            ) : (
+              <ToDoSkeleton height={55} num={3} />
+            )}
+          </div>
+          <div>
+            {topPrio ? (
+              <>
+                {topPrio.length > 0 ? <h1>High Priority Actions</h1> : null}
+
+                {topPrio.map((item) => {
+                  if (isTimestamp(item.deadline)) {
+                    if (currTime > item.deadline) {
+                      return null;
+                    }
+                  }
+                  if (item.priority === 0) {
+                    return null;
+                  }
+
+                  return <ToDoItem key={item.id} todoItem={item}></ToDoItem>;
+                })}
+              </>
+            ) : (
+              <ToDoSkeleton height={55} num={3} />
+            )}
+          </div>
+          <div>
+            <h1>Upcoming Deadlines</h1>
+            {nearDeadline ? (
               nearDeadline.map((item) => {
                 return <ToDoItem key={item.id} todoItem={item}></ToDoItem>;
               })
-              :
-              <ToDoSkeleton height={55} num={3}/>
-            }
-            </div>
-            </>
-            :
-            <h1>Please log in</h1>
-      }
-
+            ) : (
+              <ToDoSkeleton height={55} num={3} />
+            )}
+          </div>
+        </>
+      ) : (
+        <h1>Please log in</h1>
+      )}
     </Container>
   );
 };
