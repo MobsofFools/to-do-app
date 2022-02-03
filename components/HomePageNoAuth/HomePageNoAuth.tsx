8e9d3ba145b8ba88834102c7cff663d57e475c33
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-02-02 20:24:48 -0700

    Added non-auth home page

---
+++ components/HomePageNoAuth/HomePageNoAuth.tsx
@@ -0,0 +1,9 @@
+import Container from "@mui/material/Container";
+const HomePageNoAuth = () => {
+  return (
+    <Container>
+      <div style={{ display:'grid', placeItems:'center', minHeight:"80vh" }}>saasd </div>
+    </Container>
+  );
+};
+export default HomePageNoAuth;
