4c534de2931a1caa4e66332097ee4dba1c1b7e86
MobsofFools <32505517+MobsofFools@users.noreply.github.com>
2022-02-02 19:45:56 -0700

    Cookie popup to annoy people

--- components/CookiePopup/CookiePopup.tsx
+++ components/CookiePopup/CookiePopup.tsx
@@ -1,8 +1,63 @@
+import Button from "@mui/material/Button";
+import { useEffect, useState } from "react";
 const CookiePopup = () => {
-    return(
-        <div>
-            
-        </div>
+  const [popupOpen, setPopupOpen] = useState(true);
+  function getCookie(name: string) {
+    var match = document.cookie.match(
+      RegExp("(?:^|;\\s*)" + name + "=([^;]*)")
     );
-}
-export default CookiePopupp
+    return match ? match[1] : null;
+  }
+  useEffect(() => {
+    if (getCookie("portfolio_todo_2022_cookie_policy_accepted")) {
+      setPopupOpen(false);
+    }
+  }, []);
+
+  const handleAcceptCookies = () => {
+    document.cookie = "portfolio_todo_2022_cookie_policy_accepted = true";
+    setPopupOpen(false);
+  };
+  return popupOpen ? (
+    <div
+      style={{
+        position: "absolute",
+        bottom: "1rem",
+        right: "1rem",
+        maxWidth: "400px",
+        backgroundColor: "hsla(200,50%,30%,0.9)",
+        padding: "1rem",
+        borderRadius: "0.5rem",
+        color: "white"
+      }}
+    >
+      <p>
+        This website uses cookies to make your experience on this website
+        better.<br/>
+        By using this website you are agreeing to the use of cookies.
+      </p>  
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap:"wrap",
+          marginTop: "1rem",
+        }}
+      >
+        <Button variant="contained" onClick={handleAcceptCookies}>
+          I accept
+        </Button>
+        <div style={{ fontSize: "14px", color: "blue" }}>
+          <a href="/cookies" target="_blank">
+            Cookie Policies
+          </a>
+          <br />
+          <a href="/privacy" target="_blank">
+            Privacy Policies
+          </a>
+        </div>
+      </div>
+    </div>
+  ) : null;
+};
+export default CookiePopup;
